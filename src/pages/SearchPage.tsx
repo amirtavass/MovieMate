179086a72b4077266defe5b7ae13d0c17aa896d1
amirtavass <amirtavass62@gmail.com>
2025-11-04 13:37:30 +0000

    conditional rendering with ts applied in searchpage component

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,15 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, ReactNode } from "react";
 import styles from "./SearchPage.module.css";
+// @ts-ignore
 import { useMovies } from "../hooks/useMovies";
 import MovieList from "../components/movie/MovieList";
 import Loader from "../components/ui/Loader";
 import ErrorMessage from "../components/ui/ErrorMessage";
+// @ts-ignore
 import { useWatchedMovies } from "../contexts/MoviesContext";
 import { IoSearchOutline } from "react-icons/io5";
+
 function SearchResults() {
   const { query, setQuery } = useWatchedMovies();
-  const [localQuery, setLocalQuery] = useState(query);
+  const [localQuery, setLocalQuery] = useState<string>(query);
   const { movies, isLoading, error } = useMovies(query);
+
+  let content: ReactNode;
+
+  if (isLoading) {
+    content = <Loader />;
+  } else if (error) {
+    content = <ErrorMessage message={error} />;
+  } else if (!query) {
+    content = (
+      <div className={styles.welcomeMessage}>
+        <p>
+          Explore thousands of movies and add your favorites to your watched
+          list
+        </p>
+      </div>
+    );
+  } else if (movies?.length === 0) {
+    content = <p className={styles.noResults}>No movies found for "{query}"</p>;
+  } else if (movies && movies.length > 0) {
+    content = (
+      <>
+        <p className={styles.resultsCount}>
+          Found <strong>{movies.length}</strong> results for "{query}"
+        </p>
+        <MovieList movies={movies} />
+      </>
+    );
+  }
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -31,29 +62,7 @@ function SearchResults() {
           />
         </div>
       </div>
-      <div className={styles.resultContainer}>
-        {isLoading && <Loader />}
-        {error && <ErrorMessage message={error} />}
-        {!isLoading && !error && !query && (
-          <div className={styles.welcomeMessage}>
-            <p>
-              Explore thousands of movies and add your favorites to your watched
-              list
-            </p>
-          </div>
-        )}
-        {!isLoading && !error && movies?.length > 0 && (
-          <>
-            <p className={styles.resultsCount}>
-              Found <strong>{movies.length}</strong> results for "{query}"
-            </p>
-            <MovieList movies={movies} />
-          </>
-        )}
-        {!isLoading && !error && query && movies?.length === 0 && (
-          <p className={styles.noResults}>No movies found for "{query}"</p>
-        )}
-      </div>
+      <div className={styles.resultContainer}>{content}</div>
     </div>
   );
 }
